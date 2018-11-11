4c46d9b964c348afaac2fbacfb941da44552f314
charliesands <cwsands3@gmail.com>
2018-11-10 20:47:49 -0700

    setup

---
+++ NumberVariableExercises/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumberVariableExercises
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            decimal myDecimalValue = 10;
+            int myIntValue = myDecimalValue;
+            MessageBox.Show("The myIntValue is " + myIntValue);
+        }
+    }
+}
